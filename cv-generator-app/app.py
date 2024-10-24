0fe5185882763a5e31ebb8444235ee27fc26df6b
elias <elias-dev69600@gmail.com>
2024-10-24 12:42:24 +0200

    mis a jour branche - Elias Djedoui

--- cv-generator-app/app.py
+++ cv-generator-app/app.py
@@ -4,7 +4,7 @@ path = "root/pages/"
 pg = st.navigation([
         st.Page(path + "Home.py", title="Accueil", icon=":material/home:"),
         st.Page(path + "Tutorials.py", title="Guide d'utilisation", icon=":material/help:"),
-        # st.Page(path + "CVGenerator.py", title="Générer son CV", icon=":material/picture_as_pdf:"),
+        st.Page(path + "CVGenerator.py", title="Générer son CV", icon=":material/picture_as_pdf:"),
         # st.Page(path + "CoverLetterGenerator.py", title="Générer sa lettre de motivation", icon=":material/picture_as_pdf:"),
         st.Page(path + "UsingCVTemplate.py", title="Générer son CV via template", icon=":material/picture_as_pdf:"),
         st.Page(path + "CVCreator.py", title="Créer son CV"),
